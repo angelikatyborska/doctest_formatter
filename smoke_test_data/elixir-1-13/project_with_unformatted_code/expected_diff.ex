7f7912fe7d0bdd876d4c3a0b9c70bfc5b0ef5ccf
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-02-25 20:35:28 +0100

    Update smoke test

--- smoke_test_data/elixir-1-13/project_with_unformatted_code/expected_diff.ex
+++ smoke_test_data/elixir-1-13/project_with_unformatted_code/expected_diff.ex
@@ -2,7 +2,7 @@ defmodule ExpectedDiff do
   def diff do
     """
     diff --git a/smoke_test_data/elixir-1-13/project_with_unformatted_code/lib/project_with_unformatted_code.ex b/smoke_test_data/elixir-1-13/project_with_unformatted_code/lib/project_with_unformatted_code.ex
-    index 730bd33..53fdd6d 100644
+    index 730bd33..50b6379 100644
     --- a/smoke_test_data/elixir-1-13/project_with_unformatted_code/lib/project_with_unformatted_code.ex
     +++ b/smoke_test_data/elixir-1-13/project_with_unformatted_code/lib/project_with_unformatted_code.ex
     @@ -2,7 +2,7 @@ defmodule ProjectWithUnformattedCode do
@@ -25,7 +25,7 @@ defmodule ExpectedDiff do
     #{" "}
        \"""
        def add(a, b) do
-    @@ -24,10 +24,18 @@ defmodule ProjectWithUnformattedCode do
+    @@ -24,12 +24,28 @@ defmodule ProjectWithUnformattedCode do
        end
     #{" "}
        @doc \"""
@@ -44,8 +44,19 @@ defmodule ExpectedDiff do
     +  ...>   700_000_000_000
     +  ...> ]
        ...> |> Enum.map(&ProjectWithUnformattedCode.subtract(&1, 100_000_000_000))
-       [0, 100_000_000_000, 200_000_000_000, 300_000_000_000, 400_000_000_000, 500_000_000_000, 600_000_000_000]
+    -  [0, 100_000_000_000, 200_000_000_000, 300_000_000_000, 400_000_000_000, 500_000_000_000, 600_000_000_000]
+    +  [
+    +    0,
+    +    100_000_000_000,
+    +    200_000_000_000,
+    +    300_000_000_000,
+    +    400_000_000_000,
+    +    500_000_000_000,
+    +    600_000_000_000
+    +  ]
        \"""
+       def subtract(a, b) do
+         a - b
     """
   end
 end
