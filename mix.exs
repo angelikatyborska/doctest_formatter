5ddc86483ba3d49db9904bd313ebae28c767b79a
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-02-25 16:33:04 +0100

    Fix project description

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule DoctestFormatter.MixProject do
   end
 
   defp description() do
-    "Elixir formatter plugin for formatting Elixir code in Markdown files."
+    "Elixir formatter plugin for formatting Elixir code in doctests."
   end
 
   defp package() do
