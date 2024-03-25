87d2e3a5357fade6e624e2b94d1eb0698e42a03e
Angelika Tyborska <angelikatyborska@fastmail.com>
2024-03-25 19:50:53 +0100

    Ensure smoke test doctests are valid doctests

--- bin/smoke_test.exs
+++ bin/smoke_test.exs
@@ -42,6 +42,7 @@ Enum.each(projects, fn project ->
     {_, 0} = System.cmd("mix", ["compile"], cd: project_path)
   end
 
+  {_, 0} = System.cmd("mix", ["test"], cd: project_path)
   {_, 0} = System.cmd("mix", ["format"], cd: project_path)
   {diff, 0} = System.cmd("git", ["diff"], cd: project_path)
 
